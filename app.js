7e43d01c1a53b214d4c3e8193f388960a1e6ebe9
Arun <arunsun4048@gmail.com>
2018-02-16 13:11:10 +0530

    google auth

--- app.js
+++ app.js
@@ -4,9 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require("passport");
+var flash = require('connect-flash');
+var session = require('express-session');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var configDB = require('./config/database.js')
+
+// DB Configuration
+mongoose.connect(configDB.url);
+
+require('./config/passport')(passport);
 
 var app = express();
 
@@ -22,8 +32,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use(session({ secret: 'xyz'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+/*app.use('/', index);
+app.use('/users', users);*/
+
+require('./routes.js')(app, passport);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'googleAuth' : {
+        'clientID'      : '603289757487-nm8kcparl9tlp8vp5f9mhmkrvk11frdj.apps.googleusercontent.com',
+        'clientSecret'  : 'k2jZvItjLXTrsPhtKYe3akal',
+        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+    }
+}
